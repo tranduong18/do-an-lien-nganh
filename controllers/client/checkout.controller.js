ab87a82bc5d3cb027756888bddbbacfad1ab39e2
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-10-09 22:09:11 +0700

    fix tìm kiếm sản phẩm

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -184,8 +184,7 @@ module.exports.create_url_payment = async (req, res) => {
     let secretKey = vnp_HashSecret;
 
     const orderId = req.query.orderId;
-    let amount = parseFloat(req.query.amount);
-    
+    let amount = req.query.amount;
     
     const locale = 'vn';
     const currCode = 'VND';
@@ -198,7 +197,7 @@ module.exports.create_url_payment = async (req, res) => {
     vnp_Params['vnp_TxnRef'] = orderId;
     vnp_Params['vnp_OrderInfo'] = 'Thanh toan cho ma GD:' + orderId;
     vnp_Params['vnp_OrderType'] = 'other';
-    vnp_Params['vnp_Amount'] = parseInt(amount * 100000) ;
+    vnp_Params['vnp_Amount'] = parseInt(amount) * 100;
     vnp_Params['vnp_IpAddr'] = ipAddr;
     vnp_Params['vnp_ReturnUrl'] = returnUrl;
     vnp_Params['vnp_CreateDate'] = createDate;
--- controllers/client/search.controller.js
+++ controllers/client/search.controller.js
@@ -1,30 +1,44 @@
 const Product = require("../../models/product.model");
+const paginationHelper = require("../../helpers/pagination.helper");
 
 // [GET] /search
 module.exports.index = async(req, res) => {
     const keyword = req.query.keyword;
 
-    let products = [];
+    let paginatedProductsSearch = [];
+    let pagination = [];
 
     if(keyword){
         const regex = new RegExp(keyword, "i");
 
-        products = await Product.find({
-            title: regex,
+        const find = {
             status: "active",
-            deleted: false
-        });
-
-        for(const item of products){
-            item.priceNew = ((1 - item.discountPercentage/100) * item.price).toFixed(0);
+            deleted: false,
+            title: regex
+        };
+    
+        let allProducts = await Product.find(find).sort({ position: "desc" });
+    
+        // Lọc theo giá
+        if (req.query.priceStart && req.query.priceEnd) {
+            const priceStart = Number(req.query.priceStart);
+            const priceEnd = Number(req.query.priceEnd);
+    
+            allProducts = allProducts.filter(product => {
+                return product.priceNew >= priceStart && product.priceNew <= priceEnd;
+            });
         }
+    
+        // Phân trang
+        pagination = await paginationHelper.productClient(req, allProducts);
+    
+        paginatedProductsSearch = allProducts.slice(pagination.skip, pagination.skip + pagination.limitItems);
     }
-
-    // console.log(products);
 
     res.render("client/pages/search/index", {
         pageTitle: "Tìm kiếm",
         keyword: keyword,
-        products: products
+        products: paginatedProductsSearch,
+        pagination: pagination
     });
 }
