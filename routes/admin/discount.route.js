b54ab909feaa049ab1c4b26cf159e938c625b750
Sonnnnkl03 <94890686+Sonnnnkl03@users.noreply.github.com>
2024-11-04 20:43:43 +0700

    update discount

--- routes/admin/discount.route.js
+++ routes/admin/discount.route.js
@@ -1,26 +1,26 @@
-// const express = require("express");
-// const router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-// const controller = require("../../controllers/admin/discount.controller");
+const controller = require("../../controllers/admin/discount.controller");
 
-// router.get("/", controller.index);
+router.get("/", controller.index);
 
-// router.get("/create", controller.create);
+router.get("/create", controller.create);
 
-// router.post(
-//     "/create",
-//     controller.createPost
-// );
+router.post(
+    "/create",
+    controller.createPost
+);
 
-// router.get("/edit/:id", controller.edit);
+router.get("/edit/:id", controller.edit);
 
-// router.patch(
-//     "/edit/:id",
-//     controller.editPatch
-// );
+router.patch(
+    "/edit/:id",
+    controller.editPatch
+);
 
-// router.patch("/delete/:id", controller.deletePatch);
+router.patch("/delete/:id", controller.deletePatch);
 
-// router.patch("/change-status/:statusChange/:id", controller.changeStatus);
+router.patch("/change-status/:statusChange/:id", controller.changeStatus);
 
-// module.exports = router;
+module.exports = router;
